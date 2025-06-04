030ba77a1da82aac01e0a18a2c03b486076239c5
shewon1234 <shewonmelon@gmail.com>
2025-06-04 09:38:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,6 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GnJGBSjB#gKy8xjMo8xIe782IsC0Cv607tEboipPT9hwVSh4xYzw' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+SESSION_NAME: process.env.PORT === undefined ? "dew" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
 };
